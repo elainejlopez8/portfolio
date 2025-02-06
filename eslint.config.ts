8eea7557e2516fb4d955b8a36db6eb7948653aec
Elaine Lopez <elainejlopez870@gmail.com>
2025-02-06 20:06:05 +1100

    feat: update eslint

---
+++ eslint.config.ts
@@ -0,0 +1,43 @@
+import pluginJs from '@eslint/js';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
+import { default as tseslint, default as typescriptEslint } from 'typescript-eslint';
+
+export default [
+  {
+    ignores: ['*.config*'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  },
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  react.configs.flat.recommended,
+  {
+    plugins: {
+      typescriptEslint,
+      react,
+    },
+  },
+  {
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+  },
+];
