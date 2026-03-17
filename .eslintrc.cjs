f427fee2e258de76f6021fe266f6b78caf58859f
Elaine Lopez <elaine.lopez@iag.com.au>
2026-03-17 14:19:12 +1100

    chore: update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,16 @@
 // eslint-disable-next-line no-undef
 module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {
+        project: ['./tsconfig.json'],
+      },
+    },
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+  },
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
